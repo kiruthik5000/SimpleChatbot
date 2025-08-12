0f2aadfbc053b38c9da568d6c146c9ae204ada3d
kiruthik5000 <kiruthikvairam5000@gmail.com>
2025-08-12 18:57:43 +0530

    Created a chatbot

---
+++ UI/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // <-- Required for React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
